71bfc4b712a4b8e46e95e34f3606db78bf11323c
unknown <joaofs901@gmail.com>
2024-10-04 16:05:01 -0300

    fix problem in generate page tickets

--- src/pages/ListTickets.jsx
+++ src/pages/ListTickets.jsx
@@ -1,4 +1,3 @@
-// ListTickets.jsx
 import { Link } from "react-router-dom";
 import ButtonComponent from "../components/ButtonComponent";
 import H2Component from "../components/H2Component";
@@ -12,15 +11,18 @@ const ListTickets = () => {
   const [criacao, setCriacao] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [totalTickets, setTotalTickets] = useState(0);
+  const [isLoading, setIsLoading] = useState(true); // Estado para controlar o carregamento
 
   // Função para buscar os tickets com paginação
   const fetchTickets = () => {
+    setIsLoading(true); // Ativa o estado de carregamento antes de buscar os tickets
     apiService.getTickets(operacao, sequencia, criacao, currentPage, 5)
       .then(response => {
         setTickets(response.data.results);
         setTotalTickets(response.data.count);
       })
-      .catch(error => console.error('Erro ao buscar os tickets:', error));
+      .catch(error => console.error('Erro ao buscar os tickets:', error))
+      .finally(() => setIsLoading(false)); // Desativa o estado de carregamento após buscar os tickets
   };
 
   useEffect(() => {
@@ -40,75 +42,81 @@ const ListTickets = () => {
   return (
     <div className="min-h-screen w-full bg-slate-800 rounded-md shadow flex flex-col text-white">
       <H2Component title="Tickets" />
-      <table className="border-separate border border-slate-500 py-3">
-        <thead>
-          <tr>
-            <th className="border border-slate-800 bg-slate-900">Nº Ticket</th>
-            <th className="border border-slate-800 bg-slate-900">Placa</th>
-            <th className="border border-slate-800 bg-slate-900">Produto</th>
-            <th className="border border-slate-800 bg-slate-900">Transportadora</th>
-            <th className="border border-slate-800 bg-slate-900">Cliente</th>
-            <th className="border border-slate-800 bg-slate-900">Peso Líquido</th>
-            <th className="border border-slate-800 bg-slate-900">Lote Leira</th>
-            <th className="border border-slate-800 bg-slate-900">Opções</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tickets.map((ticket) => (
-            <tr key={ticket.id}>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.sequencia}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.placa}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.empresa.nome}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.transportadora}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.cliente}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.peso_liquido}</td>
-              <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.criacao}</td>
-              <td className="border-slate-700"><Link  setSelectedTicketId ={ticket.id} to={"/print"}><ButtonComponent  color="bg-slate-800" nameButton="Imprimir" /></Link></td>
+
+      {/* Spinner de Carregamento */}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-32">
+          <div className="w-12 h-12 border-4 border-t-4 border-t-transparent border-white rounded-full animate-spin"></div>
+          <span className="ml-2">Carregando tickets...</span>
+        </div>
+      ) : (
+        <table className="border-separate border border-slate-500 py-3">
+          <thead>
+            <tr>
+              <th className="border border-slate-800 bg-slate-900">Nº Ticket</th>
+              <th className="border border-slate-800 bg-slate-900">Placa</th>
+              <th className="border border-slate-800 bg-slate-900">Produto</th>
+              <th className="border border-slate-800 bg-slate-900">Transportadora</th>
+              <th className="border border-slate-800 bg-slate-900">Cliente</th>
+              <th className="border border-slate-800 bg-slate-900">Peso Líquido</th>
+              <th className="border border-slate-800 bg-slate-900">Lote Leira</th>
+              <th className="border border-slate-800 bg-slate-900">Opções</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tickets.map((ticket) => (
+              <tr key={ticket.id}>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.sequencia}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.placa}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.empresa.nome}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.transportadora}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.cliente}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.peso_liquido}</td>
+                <td className="border border-slate-700 hover:bg-slate-500 text-center">{ticket.criacao}</td>
+                <td className="border-slate-700">
+                  <Link to={"/print"} state={{ ticketId: ticket.id }}>
+                    <ButtonComponent color="bg-slate-800" nameButton="Imprimir" />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
 
       {/* Componente de Paginação */}
-      <div className="flex p-4 text-center justify-end space-x-2">
-        {/* Botão para ir para a primeira página */}
-        <ButtonComponent
-          nameButton="Início"
-          onClick={() => handlePageChange(1)}
-          disabled={currentPage === 1}
-        />
-        
-        {/* Botão para página anterior */}
-        <ButtonComponent
-          nameButton="Anterior"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        />
-
-        {/* Botões de Paginação Paginados */}
-        {Array.from({ length: totalPages }, (_, index) => (
+      {!isLoading && (
+        <div className="flex p-4 text-center justify-end space-x-2">
           <ButtonComponent
-            key={index + 1}
-            nameButton={(index + 1).toString()}
-            onClick={() => handlePageChange(index + 1)}
-            active={currentPage === index + 1} // Indicar a página ativa
+            nameButton="Início"
+            onClick={() => handlePageChange(1)}
+            disabled={currentPage === 1}
           />
-        ))}
-
-        {/* Botão para próxima página */}
-        <ButtonComponent
-          nameButton="Próxima"
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        />
-
-        {/* Botão para ir para a última página */}
-        <ButtonComponent
-          nameButton="Fim"
-          onClick={() => handlePageChange(totalPages)}
-          disabled={currentPage === totalPages}
-        />
-      </div>
+          <ButtonComponent
+            nameButton="Anterior"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          />
+          {Array.from({ length: totalPages }, (_, index) => (
+            <ButtonComponent
+              key={index + 1}
+              nameButton={(index + 1).toString()}
+              onClick={() => handlePageChange(index + 1)}
+              active={currentPage === index + 1} 
+            />
+          ))}
+          <ButtonComponent
+            nameButton="Próxima"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          />
+          <ButtonComponent
+            nameButton="Fim"
+            onClick={() => handlePageChange(totalPages)}
+            disabled={currentPage === totalPages}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/TicketPrint.jsx
+++ src/pages/TicketPrint.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import apiService from '../services/apiService';
 import './css/TicketPrint.css'; // Importa um arquivo CSS para estilização
 import imagem from '../assets/logo-organics.png';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ChevronLeftIcon } from 'lucide-react';
 
-const TicketPrint = ({ ticketId }) => {
+const TicketPrint = () => {
   const [ticket, setTicket] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+  const { ticketId } = location.state || {}; // Obtém o ticketId
+
+    const navigate = useNavigate();
 
   useEffect(() => {
     if (ticketId) {
-      apiService
-        .getTicketById(ticketId)
+      apiService.getTicketById(ticketId)
         .then(response => {
           setTicket(response.data);
         })
@@ -26,118 +30,128 @@ const TicketPrint = ({ ticketId }) => {
 
   return (
     <div>
+    <div>
+      <button onClick={()=> navigate(-1)} className='bg-slate-100 rounded-md py-1 px-2'>
+        <ChevronLeftIcon/>
+      </button>
+    </div>
       <div className="ticket-print-container">
-      <div className='logo-impressao'>
-        <img src={imagem} alt="organics-logo" width={160} height={124} />
-        <h2 className="ticket-title"> {ticket.empresa.nome}</h2>
-      </div>
-      <h4 className='ticket-title'>Endereço: {ticket.empresa.endereco}</h4>
-      <div className="ticket-details">
-        <div className="ticket-item">
-          <strong>Data de criação:</strong> <strong>{new Date(ticket.criacao).toLocaleDateString()}</strong>
+        <div className="logo-impressao">
+          <img src={imagem} alt="organics-logo" width={160} height={124} />
+          <h2 className="ticket-title"> {ticket.empresa.nome}</h2>
         </div>
-        <div className="ticket-item">
-          <strong>Sequência:</strong> <span>{ticket.sequencia}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Operador:</strong> <span>{ticket.operador}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Motorista:</strong> <span>{ticket.motorista}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Placa:</strong> <span>{ticket.placa}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Operação:</strong> <span>{ticket.empresa.nome}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Transportadora:</strong> <span>{ticket.transportadora}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Produto:</strong> <span>{ticket.produto}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Cliente:</strong> <span>{ticket.cliente}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Entrada:</strong> <span>{ticket.peso_entrada} Ton</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Saída:</strong> <span>{ticket.peso_saida} Ton</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Líquido:</strong> <span>{ticket.peso_liquido} Ton</span>
-        </div>
-        <div className='assinatura'>
-          <div className="assinatura-item">
-            <strong>Assinatura Motorista:</strong>
-            <span>________________________________________ </span>
+        <h4 className="ticket-title">Endereço: {ticket.empresa.endereco}</h4>
+        <div className="ticket-details">
+          <div className="ticket-item">
+            <strong>Data de criação:</strong> <strong>{new Date(ticket.criacao).toLocaleDateString()}</strong>
           </div>
-          <div className="assinatura-item">
-            <strong>Assinatura Operador:</strong>
-            <span>________________________________________</span>
+          <div className="ticket-item">
+            <strong>Sequência:</strong> <span>{ticket.sequencia}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Operador:</strong> <span>{ticket.operador}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Motorista:</strong> <span>{ticket.motorista}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Placa:</strong> <span>{ticket.placa}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Operação:</strong> <span>{ticket.empresa.nome}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Transportadora:</strong> <span>{ticket.transportadora}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Produto:</strong> <span>{ticket.produto}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Cliente:</strong> <span>{ticket.cliente}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Entrada:</strong> <span>{ticket.peso_entrada} Ton</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Saída:</strong> <span>{ticket.peso_saida} Ton</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Líquido:</strong> <span>{ticket.peso_liquido} Ton</span>
+          </div>
+          <div className="assinatura">
+            <div className="assinatura-item">
+              <strong>Assinatura Motorista:</strong>
+              <span>________________________________________ </span>
+            </div>
+            <div className="assinatura-item">
+              <strong>Assinatura Operador:</strong>
+              <span>________________________________________</span>
+            </div>
           </div>
         </div>
       </div>
 
-    </div>
-    <div className="ticket-print-container">
-      <div className='logo-impressao'>
-        <img src={imagem} alt="organics-logo" width={160} height={124} />
-        <h2 className="ticket-title"> {ticket.empresa.nome}</h2>
-      </div>
-      <h4 className='ticket-title'>Endereço: {ticket.empresa.endereco}</h4>
-      <div className="ticket-details">
-        <div className="ticket-item">
-          <strong>Data de criação:</strong> <strong>{new Date(ticket.criacao).toLocaleDateString()}</strong>
+      {/* Segundo bloco de impressão */}
+      <div className="ticket-print-container">
+        <div className="logo-impressao">
+          <img src={imagem} alt="organics-logo" width={160} height={124} />
+          <h2 className="ticket-title"> {ticket.empresa.nome}</h2>
         </div>
-        <div className="ticket-item">
-          <strong>Sequência:</strong> <span>{ticket.sequencia}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Operador:</strong> <span>{ticket.operador}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Motorista:</strong> <span>{ticket.motorista}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Placa:</strong> <span>{ticket.placa}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Operação:</strong> <span>{ticket.empresa.nome}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Transportadora:</strong> <span>{ticket.transportadora}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Produto:</strong> <span>{ticket.produto}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Cliente:</strong> <span>{ticket.cliente}</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Entrada:</strong> <span>{ticket.peso_entrada} Ton</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Saída:</strong> <span>{ticket.peso_saida} Ton</span>
-        </div>
-        <div className="ticket-item">
-          <strong>Peso Líquido:</strong> <span>{ticket.peso_liquido} Ton</span>
-        </div>
-        <div className='assinatura'>
-          <div className="assinatura-item">
-            <strong>Assinatura Motorista:</strong>
-            <span>________________________________________ </span>
+        <h4 className="ticket-title">Endereço: {ticket.empresa.endereco}</h4>
+        <div className="ticket-details">
+          {/* Os mesmos itens de detalhes do ticket repetidos para segunda impressão */}
+          <div className="ticket-item">
+            <strong>Data de criação:</strong> <strong>{new Date(ticket.criacao).toLocaleDateString()}</strong>
           </div>
-          <div className="assinatura-item">
-            <strong>Assinatura Operador:</strong>
-            <span>________________________________________</span>
+          <div className="ticket-item">
+            <strong>Sequência:</strong> <span>{ticket.sequencia}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Operador:</strong> <span>{ticket.operador}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Motorista:</strong> <span>{ticket.motorista}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Placa:</strong> <span>{ticket.placa}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Operação:</strong> <span>{ticket.empresa.nome}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Transportadora:</strong> <span>{ticket.transportadora}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Produto:</strong> <span>{ticket.produto}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Cliente:</strong> <span>{ticket.cliente}</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Entrada:</strong> <span>{ticket.peso_entrada} Ton</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Saída:</strong> <span>{ticket.peso_saida} Ton</span>
+          </div>
+          <div className="ticket-item">
+            <strong>Peso Líquido:</strong> <span>{ticket.peso_liquido} Ton</span>
+          </div>
+          <div className="assinatura">
+            <div className="assinatura-item">
+              <strong>Assinatura Motorista:</strong>
+              <span>________________________________________ </span>
+            </div>
+            <div className="assinatura-item">
+              <strong>Assinatura Operador:</strong>
+              <span>________________________________________</span>
+            </div>
           </div>
         </div>
+        <div className='flex'>
+          <button className="btn-print">Inserir Imagem</button>
+          <button className="btn-green" onClick={handlePrint}>Imprimir Ticket</button>
+        </div>
       </div>
-      <button className="btn-print" onClick={handlePrint}>Imprimir Ticket</button>
-    </div>
     </div>
   );
 };

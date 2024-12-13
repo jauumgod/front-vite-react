1ae42085007caad51805f9ab6cb20cfdb1446830
unknown <joaofs901@gmail.com>
2024-12-13 16:49:03 -0300

    update in api file

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,8 @@
 
 
-// const API_URL = 'https://api-tickets-tp22.onrender.com/api';
+const API_URL = 'https://api-tickets-tp22.onrender.com/api';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+// const API_URL = 'http://127.0.0.1:8000/api';
 
 
 
